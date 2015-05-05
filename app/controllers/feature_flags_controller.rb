90ae9bb21c96e44792d6d34b3609830086b4407d
Andrey Mosin <mosinandrej@gmail.com>
2015-05-05 21:47:35 +0700

    Changed authorization process for feature

--- app/controllers/feature_flags_controller.rb
+++ app/controllers/feature_flags_controller.rb
@@ -1,17 +1,19 @@
 class FeatureFlagsController < ApplicationController
   layout FeatureFlags.configuration.layout.downcase
   before_filter :load_features, :only => [:index,:create,:update,:destroy]
-  load_and_authorize_resource
+  before_filter :get_feature, :only => [:edit, :update, :destroy]
 
   def index
+    authorize! :manage, Feature
   end
 
   def new
     @feature = Feature.new
+    authorize! :manage, Feature
   end
 
   def edit
-    @feature = Feature.find(params[:id])
+    authorize! :manage, Feature
   end
 
   def create
@@ -29,24 +31,26 @@ class FeatureFlagsController < ApplicationController
         }
       end
     end
+    authorize! :manage, Feature
   end
 
   def enable_all
     FeatureFlags.enable_all
+    authorize! :manage, Feature
   end
 
   def disable_all
     FeatureFlags.disable_all
+    authorize! :manage, Feature
   end
 
   def update 
-    feature = Feature.find(params[:id])
     enabled_all = params[:enable_all].present? ? enable_all : false
     disabled_all = params[:disable_all].present? ? disable_all : false
 
     respond_to do |format|
-      if enabled_all || disabled_all || feature.update_attributes(feature_params)  
-        flash[:notice] = "#{feature.name} feature successfully updated"
+      if enabled_all || disabled_all || @feature.update_attributes(feature_params)  
+        flash[:notice] = "#{@feature.name} feature successfully updated"
         format.html{
           redirect_to feature_flags_url
         }
@@ -55,7 +59,7 @@ class FeatureFlagsController < ApplicationController
           render :json => {:status => true, :message => flash[:notice]}
         }       
       else        
-        flash[:error] = "#{feature.name} feature could not be updated"
+        flash[:error] = "#{@feature.name} feature could not be updated"
         format.html{
           redirect_to feature_flags_url
         }
@@ -64,13 +68,12 @@ class FeatureFlagsController < ApplicationController
         }       
       end     
     end
+    authorize! :manage, Feature
   end
 
   def destroy
-    feature = Feature.find(params[:id])
-
     respond_to do |format|
-      if feature.destroy
+      if @feature.destroy
         flash[:notice] = "Feature successfully removed"
         format.html{
           redirect_to feature_flags_url
@@ -89,13 +92,21 @@ class FeatureFlagsController < ApplicationController
         }       
       end     
     end
-  end
-
-  def feature_params
-    params.require(:feature).permit(:name, :status, :disable_all, :enable_all)
+    authorize! :manage, Feature
   end
 
   def load_features
     @features = Feature.all
   end
+
+  private
+
+    def get_feature
+      @feature = Feature.find(params[:id])
+    end
+
+    def feature_params
+      params.require(:feature).permit(:name, :status, :disable_all, :enable_all)
+    end
+
 end
